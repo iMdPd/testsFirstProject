575d554db486a4545a22da9d6175964837eede53
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-28 20:44:16 +0200

    Create test to simulate User click on button

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -1,8 +1,18 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CurrencyForm from "./CurrencyForm";
 
 describe("Component CurrencyForm", () => {
   it("Should render without crashing", () => {
     render(<CurrencyForm action={() => {}} />);
+  });
+  it("should run action callback with proper data on form submit", () => {
+    const action = jest.fn();
+    render(<CurrencyForm action={action} />);
+
+    const submitButton = screen.getByText("Convert");
+    userEvent.click(submitButton);
+
+    expect(action).toHaveBeenCalledTimes(1);
   });
 });
